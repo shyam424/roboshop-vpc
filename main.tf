ad994e2f97048df6f347b91f64333cd4cb987c05
Shyam <shyamneo24@gmail.com>
2023-10-27 13:57:24 +0530

    terraform

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ module "components" {
   source    = "git::https://github.com/shyam424/tf-module-vpc.git"
   for_each  = var.vpc
   cidr = each.value["cidr"]
+  subnets = each.value["subnets"]
 
 }
 
