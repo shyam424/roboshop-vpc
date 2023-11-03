9ab5e785de83753b47dd497ab94009609a969c08
Shyam <shyamneo24@gmail.com>
2023-11-03 13:29:08 +0530

    vpc

--- vars.tf
+++ vars.tf
@@ -5,4 +5,5 @@ variable "default_vpc_route_table_id" {}
 variable "tags" {}
 variable "env" {}
 variable "lb" {}
-#variable "sg_ingress_cidr" {}
+#variable "sg_ingress_cidr" {}  #dont declare the variable unnecessarily, this will cause issues,
+#we didnt used var.sg_ingress_cidr anywhere in the roboshop-vpc which caused the issue, so commeneted the variable "sg_ingress_cidr"
