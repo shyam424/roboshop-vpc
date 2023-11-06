cf7e1d426dd7e6a480f78fa32f20bc1b5b8454fb
Shyam <shyamneo24@gmail.com>
2023-11-06 15:33:10 +0530

    vpc

--- main.tf
+++ main.tf
@@ -37,6 +37,11 @@ module "docdb" {
 
   for_each                   = var.docdb
   subnet_ids                 = local.db_subnets
+  backup_retention_period =  each.value ["backup_retention_period"]
+  preferred_backup_window =  each.value ["preferred_backup_window"]
+  skip_final_snapshot     =  each.value ["skip_final_snapshot"]
+  vpc_id                  = vpc_id
+  sg_ingress_cidr         = local.app_subnets_cidr
 }
 
 #output which is available in the main terraform code we call it as printing
